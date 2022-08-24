731317e0eb07fa5f781be45a56f246f579d6b7a6
Jorge Henrique <jorge.henrique@comvanilla.com.br>
2022-08-23 21:44:30 -0300

    Additing Files to Custscene Intro

---
+++ .history/Assets/Scripts/Cutscenes/LoadScreen_20220818010637.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScreen : MonoBehaviour
+{
+    public Image skullImage;
+    void Start()
+    {
+        StartCoroutine(FadeImage(true));
+    }
+
+     
+   
+ 
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                skullImage.color = new Color(1, 1, 1, i);
+
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(2f);
+
+            StartCoroutine(FadeImage(false));
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                skullImage.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(2f);
+
+            StartCoroutine(FadeImage(true));
+        }
+    }
+}
---
+++ .history/Assets/Scripts/GameController_20220812030023.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject selectedGun;
+    public GameObject gun;
+    private Sprite gunSprite;
+    
+
+    private Player player;
+
+    public Sprite[] spritesPlayerHud;
+
+
+    public Image lifeBar;
+    public Image stamineBar;
+    public Image healthGunBar;
+
+    public Image playerHud;
+    public Text coinText;
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public int totalEnemies = 0;
+    public bool enableSpawEnemy;
+    public int currentLevel;
+    
+    private int coin;
+    private bool hasUsingWeapon = false;
+    //private List<string> bossLevelsList = ["CaveFearBoss"];
+    string[] bossLevelsList = new string[] {"CaveFearBoss"} ; 
+
+    void Start()
+    {
+        
+
+        if (SceneManager.GetActiveScene().name == "RoomBob")
+        {
+            player = FindObjectOfType<Player>();
+        }
+        else 
+        {
+            player = FindObjectOfType<Player>();
+
+            currentLevel = PlayerPrefs.GetInt("Level");
+            Debug.Log(currentLevel);
+
+            coin = 0;
+            coinText.text = "00" + coin.ToString();
+            gunSprite = selectedGun.GetComponent<SpriteRenderer>().sprite;
+            gun.GetComponent<SpriteRenderer>().sprite = gunSprite;
+            playerHud.sprite = spritesPlayerHud[0];
+        }
+         
+    }
+
+    void Update()
+    {   
+        if(player.GetComponent<Player>().health <= 5f)
+        {
+            playerHud.sprite = spritesPlayerHud[1];
+        }
+    }
+
+    public void GameOver()
+    {
+        if(gameHasEnded == false)
+        {
+            PlayerPrefs.SetInt("Level", 0);
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+
+        
+    }
+
+
+    public void GetHit(float damage)
+    {
+        if (lifeBar.fillAmount > 0)
+        {
+
+            
+            lifeBar.fillAmount -= 0.1f * damage;
+
+        }
+        
+    }
+
+    public void UseEstamine()
+    {
+        if (stamineBar.fillAmount >= 0)
+        {
+            stamineBar.fillAmount -= 0.5f;
+            StartCoroutine(UpdateStamine());
+        }
+    }
+
+
+    public void UseHealthWeapon()
+    {
+        if (healthGunBar.fillAmount >= 0)
+        {
+            healthGunBar.fillAmount -= 0.1f;
+
+            if(hasUsingWeapon == false)
+            {
+                hasUsingWeapon = true;
+                StartCoroutine(UpdateHeathGun());
+            }
+            
+        }
+    }
+
+
+    public void AddCoin()
+    {
+        coin += 1;
+
+        if (coin / 10f < 1f)
+        {
+            coinText.text = "00" + coin.ToString();
+        }
+        else if (coin / 100f < 1f)
+        {
+            coinText.text = "0" + coin.ToString();
+        }
+        else
+        {
+            coinText.text = coin.ToString();
+        }
+        
+    }
+
+
+    public void NextLevel()
+    {
+        if(currentLevel == 1)
+        {
+            SceneManager.LoadScene(bossLevelsList[0]);
+        }
+
+        else
+        {
+            SceneManager.LoadScene("CaveGenLvL01");
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("RoomBob");
+    }
+
+
+
+    IEnumerator UpdateStamine()
+    {
+        yield return new WaitForSeconds(5f);
+
+        while(stamineBar.fillAmount < 1f)
+        {
+            player.GetComponent<Player>().stamine += 1;
+            stamineBar.fillAmount += 0.1f;
+            yield return new WaitForSeconds(5f);
+        }
+        
+    }
+
+
+    IEnumerator UpdateHeathGun()
+    {
+        yield return new WaitForSeconds(4f);
+
+        while(healthGunBar.fillAmount < 1f)
+        {
+            healthGunBar.fillAmount += 0.1f;
+            yield return new WaitForSeconds(1.5f);
+        }
+
+        if (healthGunBar.fillAmount >= 1f)
+        {
+            hasUsingWeapon = false;
+        }
+        
+    }
+
+    
+}
